dafde100b042fd5b2b905f0a02ed1a101143f475
renguoqing <1002674966@qq.com>
2018-06-27 14:14:42 +0800

    提交文件

--- src/main/java/com/kafka/KafkaConsumerMessage.java
+++ src/main/java/com/kafka/KafkaConsumerMessage.java
@@ -14,44 +14,45 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class KafkaConsumerMessage {
     private KafkaConsumer<Integer, String> consumer;
-    public final static String TOPIC = "t_cdr";
-    
-    KafkaConsumerMessage() throws IOException{
+    public final static String             TOPIC = "t_cdr";
+
+    KafkaConsumerMessage() throws IOException {
         Properties props = new Properties();
-        InputStream in =KafkaProducer.class.getClass().getResourceAsStream("/kafkaConsumer.Properties");
+        InputStream in = KafkaConsumerMessage.class.getClass().getResourceAsStream("/kafkaConsumer.Properties");
         props.load(in);
         consumer = new KafkaConsumer<Integer, String>(props);
         ResourceBundle resource = ResourceBundle.getBundle("kafkaTopic");
-        String topicName=resource.getString("topicName");
-        String[] topicNames=topicName.split(",");
+        String topicName = resource.getString("topicName");
+        String[] topicNames = topicName.split(",");
         consumer.subscribe(Arrays.asList(topicNames));//Arrays.asList("foo", "bar") 多个topic情况下通过集合处理 Collections.singletonList(topicName)
     }
-    
-    void consumer(){
+
+    void consumer() {
         Map<String, Object> topicMap = new ConcurrentHashMap<String, Object>();
         try {
             while (true) {
                 /* 读取数据，读取超时时间为1000ms */
                 ConsumerRecords<Integer, String> records = consumer.poll(1000);
                 for (ConsumerRecord<Integer, String> record : records) {
-                    System.out.printf("topic = %s,offset = %d, key = %s, value = %s", record.topic(),record.offset(), record.key(), record.value());
+                    System.out.printf("topic = %s,offset = %d, key = %s, value = %s", record.topic(), record.offset(),
+                            record.key(), record.value());
                     System.out.println();
-                    topicMap.put("topic",record.topic());
-                    topicMap.put("offset",record.offset());
-                    topicMap.put("key",record.key());
-                    topicMap.put("value",record.value());
+                    topicMap.put("topic", record.topic());
+                    topicMap.put("offset", record.offset());
+                    topicMap.put("key", record.key());
+                    topicMap.put("value", record.value());
                 }
-                if(topicMap.size()>0){
-                    System.out.printf("topicMap=%s",topicMap);
+                if (topicMap.size() > 0) {
+                    System.out.printf("topicMap=%s", topicMap);
                     System.out.println("将数据以接口的形式推送出去");
                     topicMap.clear();
                 }
             }
-        }finally {
+        } finally {
             consumer.close();
         }
     }
-    
+
     public static void main(String[] args) throws IOException {
         new KafkaConsumerMessage().consumer();
     }
--- src/main/java/com/kafka/KafkaProducer.java
+++ src/main/java/com/kafka/KafkaProducer.java
@@ -10,37 +10,36 @@ import java.util.Properties;
 import java.util.Scanner;
 
 public class KafkaProducer {
-    public Producer<String,String> producer;
-    public String TOPIC = "t_cdr";
-    String key="1000";
-    
-    public KafkaProducer() throws IOException{
+    public Producer<String, String> producer;
+    public String                   TOPIC = "t_cdr";
+    String                          key   = "1000";
+
+    public KafkaProducer() throws IOException {
         Properties props = new Properties();
-//        ResourceBundle resource = ResourceBundle.getBundle("kafka");
-        InputStream in =KafkaProducer.class.getClass().getResourceAsStream("/kafkaProducer.Properties");
+        InputStream in = KafkaProducer.class.getClass().getResourceAsStream("/kafkaProducer.Properties");
         props.load(in);
         producer = new Producer<String, String>(new ProducerConfig(props));
     }
-    
-    public KafkaProducer(String key,String topic,String msg) throws IOException{
+
+    public KafkaProducer(String key, String topic, String msg) throws IOException {
         this();
-        this.key=key;
-        this.TOPIC=topic;
+        this.key = key;
+        this.TOPIC = topic;
         produce(msg);
     }
-    
+
     void produce(String data) {
-//            String data = "hello kafka message OK";
-        KeyedMessage<String, String> s = new KeyedMessage<String,String>(TOPIC, key, data);
+        KeyedMessage<String, String> s = new KeyedMessage<String, String>(TOPIC, key, data);
         producer.send(s);
         producer.close();
     }
+
     public static void main(String[] args) throws IOException {
-        new KafkaProducer("10","t_cdr","welcome to kafka!");
+        new KafkaProducer("10", "t_cdr", "welcome to kafka!");
         while (true) {
-            Scanner sc=new Scanner(System.in);
-            String data=sc.nextLine();
-            if(data!=null) {
+            Scanner sc = new Scanner(System.in);
+            String data = sc.nextLine();
+            if (data != null) {
                 new KafkaProducer().produce(data);
             }
         }
